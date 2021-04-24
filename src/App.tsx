550df5921106260107beb9fd9a18aab8db34bdff
Angelo Chaves <angeloechaves@gmail.com>
2021-04-24 17:35:33 -0300

    Removed base monaco editor package, switched to using @monaco-editor/react. It is much more straight forward to use in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 import Display from './components/Display'
-import Editor from './components/Editor'
 import Sidebar from './components/sidebar/Sidebar'
 import SidebarButton from './components/sidebar/SidebarButton'
 import { data as ticketStructure } from './ticket-examples/ticket_structure_example.json'
+import Editor, { EditorProps } from '@monaco-editor/react'
 
 function App() {
   const [editorText, setEditorText] = useState('')
 
-  function onChangeEditorText(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setEditorText(event.target.value)
+  const onChangeEditorText = (value: string | undefined) => {
+    setEditorText(value || '')
   }
 
   function loadExample() {
@@ -21,7 +21,7 @@ function App() {
       <Sidebar>
         <SidebarButton onClick={loadExample}>Load Example</SidebarButton>
       </Sidebar>
-      <Editor className='flex flex-grow h-screen w-1/2' value={editorText} onChange={onChangeEditorText} />
+      <Editor className='flex flex-grow h-screen w-1/2' defaultLanguage='xml' value={editorText} onChange={onChangeEditorText} />
       <Display className='flex flex-grow justify-center h-screen w-1/2' value={editorText} />
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
-/*
- In Vite's current version (v2.1.2): 
- Uncaught ReferenceError: window is not defined is raised on page load when using monaco editor.
- So build.rollupOptions.output.manualChunks needs to be added until the issue is fixed.
- See: https://github.com/vitejs/vite/issues/1927#issuecomment-805803918
-*/
-const prefix = `monaco-editor/esm/vs`;
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          jsonWorker: [`${prefix}/language/json/json.worker`],
-          cssWorker: [`${prefix}/language/css/css.worker`],
-          htmlWorker: [`${prefix}/language/html/html.worker`],
-          tsWorker: [`${prefix}/language/typescript/ts.worker`],
-          editorWorker: [`${prefix}/editor/editor.worker`],
-        },
-      },
-    },
-  },
+  plugins: [reactRefresh()]
 })
