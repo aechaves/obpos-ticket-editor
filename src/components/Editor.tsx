095b5fb31c1a9733b2ebee11759cf042fbf9af2a
Angelo Chaves <angeloechaves@gmail.com>
2021-04-24 17:22:58 -0300

    Added monaco editor package. Changed Editor component to use monaco instead of a plain textarea

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,29 @@
-import React, { Component } from 'react'
+import React, { useRef, useEffect } from 'react';
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+// @ts-ignore
+self.MonacoEnvironment = {
+  getWorker(_: any, label: string) {
+    if (label === 'json') {
+      return new jsonWorker()
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return new cssWorker()
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return new htmlWorker()
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new tsWorker()
+    }
+    return new editorWorker()
+  }
+}
 
 interface EditorProps {
   value: string
@@ -6,20 +31,21 @@ interface EditorProps {
   className: string
 }
 
-interface EditorState { }
+const Editor: React.FC<EditorProps> = ({ value, onChange, className }) => {
+  const divEl = useRef<HTMLDivElement>(null);
+  let editor: monaco.editor.IStandaloneCodeEditor;
+  useEffect(() => {
+    if (divEl.current) {
+      editor = monaco.editor.create(divEl.current, {
+        value: value,
+        language: 'xml'
+      });
+    }
+    return () => {
+      editor.dispose();
+    };
+  }, []);
+  return <div className={className} ref={divEl}></div>;
+};
 
-export default class Editor extends Component<EditorProps, EditorState> {
-
-  constructor(props: EditorProps) {
-    super(props);
-  }
-
-  render() {
-    const { className, value, onChange } = this.props;
-    return (
-      <div className={className}>
-        <textarea className='w-full h-full bg-gray-100 px-2 shadow-sm block focus:outline-none border-gray-300 rounded-md' value={value} onChange={onChange} />
-      </div>
-    )
-  }
-}
+export default Editor
