510b480e4e2159e838ee668b61d71e8420ad0051
Angelo Chaves <angeloechaves@gmail.com>
2021-04-24 17:52:31 -0300

    Dark theme for the editor

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Display from './components/Display'
 import Sidebar from './components/sidebar/Sidebar'
 import SidebarButton from './components/sidebar/SidebarButton'
 import { data as ticketStructure } from './ticket-examples/ticket_structure_example.json'
-import Editor, { EditorProps } from '@monaco-editor/react'
+import Editor from '@monaco-editor/react'
 
 function App() {
   const [editorText, setEditorText] = useState('')
@@ -21,7 +21,7 @@ function App() {
       <Sidebar>
         <SidebarButton onClick={loadExample}>Load Example</SidebarButton>
       </Sidebar>
-      <Editor className='flex flex-grow h-screen w-1/2' defaultLanguage='xml' value={editorText} onChange={onChangeEditorText} />
+      <Editor className='flex flex-grow h-screen w-1/2' defaultLanguage='xml' theme='vs-dark' value={editorText} onChange={onChangeEditorText} />
       <Display className='flex flex-grow justify-center h-screen w-1/2' value={editorText} />
     </div>
   )
